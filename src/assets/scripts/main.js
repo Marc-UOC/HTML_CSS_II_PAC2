ebfe7b002257412cba6ba578b9413b31ee3ce09b
Marc Obregon Pantebre <mobregonp@uoc.edu>
2025-04-17 21:25:07 +0200

    Fixed BEM methodology

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -3,8 +3,7 @@
  * see commented examples below
  */
 
-// import 'some-node-module';
-// import SomeModule from 'some-node-module';
+import "@fortawesome/fontawesome-free/css/all.css";
 
 /**
  * Write any other JavaScript below
